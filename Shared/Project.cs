4cae87e57f78414338947b14ed7ccce5f67653da
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-12-01 11:43:31 +0100

    Radzen set-up and a little bit of site skeleton creation.

---
+++ Shared/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBank.Shared
+{
+    // TODO: Delete me
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Tags { get; set; } 
+    }
+}
---
+++ Shared/TagGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBank.Shared
+{
+    public class TagGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
