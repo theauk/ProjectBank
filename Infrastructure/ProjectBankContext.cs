de94430b67a83ed60d521e9bf32aece28b3db615
Gustav Metnik-Beck <gustav.mb@outlook.com>
2021-12-02 16:26:21 +0100

    Created docker-compose to host server/database
    Co-authored-by: sejerejen <sejerejen@users.noreply.github.com>
    Co-authored-by: Mai Sigurd Adelheid Odgaard <Mai-Sigurd@users.noreply.github.com>

--- Infrastructure/ProjectBankContext.cs
+++ Infrastructure/ProjectBankContext.cs
@@ -10,11 +10,6 @@ namespace Infrastructure
 
         public ProjectBankContext(DbContextOptions<ProjectBankContext> options) : base(options) {}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql();
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<University>(e => {
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,16 +1,24 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Identity.Web;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddKeyPerFile("/run/secrets", optional: true);
+builder.Configuration.AddEnvironmentVariables();
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ProjectBankContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("ProjectBank")));
+builder.Services.AddScoped<IProjectBankContext, ProjectBankContext>();
+
+//builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ProjectBankContext>(options => options.UseNpgsql());
+//builder.Services.AddDbContext<ProjectBankContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("ProjectBank")));
 
 var app = builder.Build();
 
