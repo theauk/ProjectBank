f58f36d76de448fb68b259bc5e3cd318c20932ac
itu <maod@itu.dk>
2021-12-05 16:40:09 +0100

    removed tag controller

--- ProjectBank.Server/Controllers/TagGroupController.cs
+++ ProjectBank.Server/Controllers/TagGroupController.cs
@@ -50,6 +50,20 @@ public class TagGroupController : ControllerBase
     {
         throw new NotImplementedException();
     }
+    
+    [Authorize(Roles = "Admin")] 
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTag(int taggroupid, int tagid)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Authorize(Roles = "Admin, Supervisor")]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PostTag(int taggroupid, [FromBody] TagCreateDTO tag)
+    {
+        throw new NotImplementedException();
+    }
 
 
 
