076cc393654a610d3c53572451d5da1c0b86fa3c
Thea U. Kjeldsmark <49352969+theauk@users.noreply.github.com>
2021-12-05 11:31:57 +0100

    Update roles front end (#5)
    * Change from policy to roles for front end account roles
    
    * Delete duplicate build-and-test.yml

--- ProjectBank.Client/Program.cs
+++ ProjectBank.Client/Program.cs
@@ -13,17 +13,13 @@ builder.Services.AddHttpClient("ProjectBank.ServerAPI", client => client.BaseAdd
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProjectBank.ServerAPI"));
 
-builder.Services.AddAuthorizationCore(config =>
-{
-    config.AddPolicy("Student", policy => policy.RequireClaim("Roles","[\"Student\"]"));
-    config.AddPolicy("Supervisor", policy => policy.RequireClaim("Roles","[\"Supervisor\"]"));
-    config.AddPolicy("Admin", policy => policy.RequireClaim("Roles","[\"Admin\"]"));
-});
-
-builder.Services.AddMsalAuthentication(options =>
+builder.Services.AddMsalAuthentication<RemoteAuthenticationState, CustomUserAccount>(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("api://23674461-24c3-4eb4-bcf7-48940c96d1cf/API.Access");
-});
+    options.UserOptions.RoleClaim = "appRole"; 
+})
+.AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, CustomUserAccount,
+    CustomAccountFactory>();
 
 await builder.Build().RunAsync();
