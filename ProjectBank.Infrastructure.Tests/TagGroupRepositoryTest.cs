5bcaf0d15ece253f9347a9277ea854bf80c32536
itu <maod@itu.dk>
2021-12-05 15:44:31 +0100

    Add packages

---
+++ ProjectBank.Infrastructure.Tests/TagGroupRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProjectBank.Infrastructure.Repositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace ProjectBank.Infrastructure.Tests;
+
+public class TagGroupRepositoryTest : IDisposable
+{
+    private readonly ProjectBankContext _context;
+    private readonly TagGroupRepository _repository;
+    private bool disposedValue;
+
+    public TagGroupRepositoryTest()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ProjectBankContext>();
+        builder.UseSqlite(connection);
+        var context = new ProjectBankContext(builder.Options);
+        context.Database.EnsureCreated();
+        
+        
+    }
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
