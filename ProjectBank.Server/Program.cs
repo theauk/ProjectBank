17c6e22c87765fac04c1447d0f61643da34b0ab7
Gustav Metnik-Beck <gustav.mb@outlook.com>
2021-12-08 14:37:18 +0100

    Seeding of database ensured

--- ProjectBank.Server/Program.cs
+++ ProjectBank.Server/Program.cs
@@ -60,9 +60,9 @@ app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
 
-/*if (!app.Environment.IsEnvironment("Integration"))
+if (!app.Environment.IsEnvironment("Integration"))
 {
     await app.SeedAsync();
-}*/
+}
 
 app.Run();
