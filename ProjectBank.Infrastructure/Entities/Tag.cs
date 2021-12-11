10816f4abada1ac936a5e87bee4586c0752b203d
sejerejen <74768960+sejerejen@users.noreply.github.com>
2021-12-11 18:01:52 +0100

    remove unnecessary icomparable implementation

--- ProjectBank.Infrastructure/Entities/Tag.cs
+++ ProjectBank.Infrastructure/Entities/Tag.cs
@@ -1,6 +1,6 @@
 namespace ProjectBank.Infrastructure.Entities
 {
-    public class Tag : IComparable<Tag>, IComparable
+    public class Tag 
 
     {
         public int Id { get; set; }
@@ -10,18 +10,6 @@ namespace ProjectBank.Infrastructure.Entities
         public TagGroup TagGroup { get; set; }
 
         public ICollection<Project> Projects { get; set; } = new List<Project>();
-
-        public int CompareTo(Tag tag) => string.Compare(Value, tag.Value);
-
-        public int CompareTo(object obj)
-        {
-            if (obj == null) return 1;
-
-            Tag otherTag = obj as Tag;
-            if (otherTag != null)
-                return CompareTo(otherTag);
-            throw new ArgumentException("Object is not a Tag");
-        }
 
     }
 }
