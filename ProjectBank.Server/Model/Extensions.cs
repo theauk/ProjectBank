52d098bfbdb1a3bcf445359828721e8617daf737
Gustav Metnik-Beck <gustav.mb@outlook.com>
2021-12-12 18:50:37 +0100

    Updated status codes to use correct ones. Removed created

--- ProjectBank.Server/Model/Extensions.cs
+++ ProjectBank.Server/Model/Extensions.cs
@@ -4,11 +4,10 @@ public static class Extensions
 {
     public static IActionResult ToActionResult(this Response response) => response switch
     {
-        Response.Updated => new AcceptedResult(),
-        Response.Deleted => new AcceptedResult(),
+        Response.Updated => new NoContentResult(),
+        Response.Deleted => new NoContentResult(),
         Response.NotFound => new NotFoundResult(),
         Response.Conflict => new ConflictResult(),
-        Response.Created => new AcceptedResult(),
         Response.BadRequest => new BadRequestResult(),
         
         _ => throw new NotSupportedException($"{response} not supported")
