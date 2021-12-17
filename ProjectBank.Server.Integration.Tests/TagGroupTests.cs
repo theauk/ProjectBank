2372a8e9193d7663a5c8e9fb1778faf51e2b672b
itu <maod@itu.dk>
2021-12-17 21:33:52 +0100

    revert tests

--- ProjectBank.Server.Integration.Tests/TagGroupTests.cs
+++ ProjectBank.Server.Integration.Tests/TagGroupTests.cs
@@ -5,7 +5,6 @@ using Blazorise.Extensions;
 using ProjectBank.Core.DTOs;
 
 namespace ProjectBank.Server.Integration.Tests;
-
 public class TagGroupTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly CustomWebApplicationFactory _factory;
@@ -21,34 +20,11 @@ public class TagGroupTests : IClassFixture<CustomWebApplicationFactory>
         var provider = TestClaimsProvider.WithAdminClaims();
         var client = _factory.CreateClientWithTestAuth(provider);
 
-        var response = await client.GetFromJsonAsync<IReadOnlyCollection<TagGroupDTO>>("api/TagGroup");
+        var response = await client.GetFromJsonAsync<IReadOnlyCollection<ProjectDTO>>("api/TagGroup");
 
         Assert.NotNull(response);
         Assert.Collection(response,
-            project => Assert.Equal(new TagGroupDTO() {  }, project)
+            project => Assert.Equal(new ProjectDTO {  }, project)
         );
     }
-    
-    [Fact]
-        public async Task updateTagGroup_with_id_1()
-        {
-            var provider = TestClaimsProvider.WithAdminClaims();
-            var client = _factory.CreateClientWithTestAuth(provider);
-
-            
-            var updated = new TagGroupUpdateDTO()
-            {
-                Id = 1,
-                Name = "Semester",
-                RequiredInProject = true,
-                SupervisorCanAddTag = false,
-                TagLimit = 2, SelectedTagValues = {"Spring 2021", "Autumn 2022"}
-            };
-            var response = await client.PutAsJsonAsync("api/TagGroup", updated);
-            Assert.Equal(response.StatusCode, HttpStatusCode.NoContent);
-    
-            var tgs = await client.GetFromJsonAsync<IReadOnlyCollection<TagGroupDTO>>("api/TagGroup");
-            var newupdated = tgs.FirstOrDefault(t => t.Id.IsEqual(1));
-            Assert.Equal(2, newupdated.TagDTOs.Count);
-        }
 }
